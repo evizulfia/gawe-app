82cd65d77e05798b18540dede3b191c67bb641b2
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-02 21:59:07 +0700

    Setup mail

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin;
+use App\Mail\Websitemail;
 use Hash;
 use Auth;
 
@@ -18,5 +19,31 @@ class AdminLoginController extends Controller
     public function forget_password()
     {
         return view('admin.forget_password');
+    }
+
+    public function login_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(Auth::guard('admin')->attempt($credential)) {
+            return redirect()->route('admin_home');
+        } else {
+            return redirect()->route('admin_login')->with('error', 'Information is not correct!');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
     }
 }
