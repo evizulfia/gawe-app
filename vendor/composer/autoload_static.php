a12926894ba8629c88a41b8e1aa9130c266388b4
Pandu Sunarya <pandusnrya@gmail.com>
2024-06-04 21:57:14 +0700

    update composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -352,8 +352,8 @@ class ComposerStaticInit57c6686e6c79a492cecc0217964dd6dc
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -459,8 +459,8 @@ class ComposerStaticInit57c6686e6c79a492cecc0217964dd6dc
         ),
         'Fruitcake\\Cors\\' => 
         array (
-            0 => __DIR__ . '/..' . '/fruitcake/php-cors/src',
-            1 => __DIR__ . '/..' . '/fruitcake/laravel-cors/src',
+            0 => __DIR__ . '/..' . '/fruitcake/laravel-cors/src',
+            1 => __DIR__ . '/..' . '/fruitcake/php-cors/src',
         ),
         'Faker\\' => 
         array (
